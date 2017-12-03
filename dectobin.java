899c339dc536f0571ed7787201aa2e7e5017691d
Jacob Paullus <34072305+PaullusJacob@users.noreply.github.com>
2017-12-03 11:34:17 -0600

    Create dectobin.java

---
+++ dectobin.java
@@ -0,0 +1,14 @@
+public class dectobin {
+    public static int convert(int dec) {
+        if ( == 0) {
+            return "0";
+        }
+        String binary = "";
+        while (n > 0) {
+            int rem = n % 2;
+            binary = rem + binary;
+            n = n / 2;
+        }
+        return binary;
+    }
+}
